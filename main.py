e0bf4d5598ed2d5f9d0c6428799138b7f7918161
yixin <yixin4@hotmail.com>
2019-08-12 00:50:43 -0400

    resize image on upload

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
-
-from PyQt5 import uic
+from PyQt5 import uic, QtCore
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtWidgets import (QApplication, QFileDialog, QInputDialog,
                              QLineEdit, QMainWindow, QWidget, QGraphicsView)
@@ -10,19 +9,23 @@ from anacam import *
 INPUT_PATH = "./input/"
 
 
-class Ui(QMainWindow):
+class MainWindow(QMainWindow):
+    resized = QtCore.pyqtSignal()
+
     def __init__(self):
-        super(Ui, self).__init__()
+        super(MainWindow, self).__init__()
         uic.loadUi('mainwindow.ui', self)
 
+        # Buttons
         self.btnUpload.clicked.connect(self.upload_image)
-        self.actionUpload.triggered.connect(self.upload_image)
-
         self.btnClear.clicked.connect(self.clear_image)
-        self.actionClear.triggered.connect(self.clear_image)
-
         self.btnAnalyze.clicked.connect(self.analyze_image)
+        # Menu actions
+        self.actionUpload.triggered.connect(self.upload_image)
+        self.actionClear.triggered.connect(self.clear_image)
         self.actionAnalyze.triggered.connect(self.analyze_image)
+
+        self.resized.connect(self.resize_image)
         self.show()
 
     def upload_image(self):
@@ -30,13 +33,18 @@ class Ui(QMainWindow):
                                            "Image files (*.jpg)")[0]
         if path:
             pixmap = QPixmap(path)
-            self.mainImage.setPixmap(pixmap)
-            # TODO: resize image
-            self.mainImage.resize(self.mainImage.width(),
-                                  self.mainImage.height())
+            pixmap_resized = pixmap.scaled(self.mainImage.width(),
+                                           self.mainImage.height(),
+                                           QtCore.Qt.KeepAspectRatio,
+                                           QtCore.Qt.FastTransformation)
+            self.mainImage.setPixmap(pixmap_resized)
 
     def clear_image(self):
         self.mainImage.clear()
+
+    def resizeEvent(self, event):
+        self.resized.emit()
+        return super(MainWindow, self).resizeEvent(event)
 
     # TODO: drag and drop functionality for imageView
 
@@ -45,6 +53,9 @@ class Ui(QMainWindow):
         hue = get_hue("", spots=3, threshold=50)
 
 
-app = QApplication(sys.argv)
-window = Ui()
-app.exec_()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    window.resize(800, 600)
+    sys.exit(app.exec_())
